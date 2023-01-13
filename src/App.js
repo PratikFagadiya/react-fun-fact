7073f958c1fe14aefef6f8acc8a937f3e985354c
Pratik Fagadiya <pratikfagadiya@gmail.com>
2023-01-13 17:00:41 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (<div>
+
+        <header>
+            <nav>
+                <img src={logo} className="App-logo" alt="logo" width={`75px`}/>
+            </nav>
+        </header>
+
+        <h1>Fun fact about React</h1>
+
+        <ul>
+            <li>Was released in 2013</li>
+            <li>Was originally created by JORDAN WALKE</li>
+            <li>Has well over 100k starts on GitHub</li>
+            <li>It is maintained by Facebook</li>
+            <li>Power thousands of enterprise apps, including mobile apps</li>
+        </ul>
+
+        <footer>
+            <small> Thanks for watching</small>
+        </footer>
+
+    </div>);
 }
 
-export default App;
+export default App
