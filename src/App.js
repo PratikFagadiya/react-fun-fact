aafd68fd4c618e848cbc74a0c8849ddc8e633bc7
Pratik Fagadiya <pratikfagadiya@gmail.com>
2023-01-16 15:38:55 +0530

    Designing Single Page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import logo from './logo.svg';
+import './App.css'
 
 const App = () => {
     return (<div>
 
-        <header>
-            <nav>
-                <img src={logo} className="App-logo" alt="logo" width={`75px`}/>
+        <Header/>
+        <MainContent/>
+        <Footer/>
+
+    </div>);
+}
+
+function Header() {
+    return (<header>
+            <nav className={'nav'}>
+                <img
+                    src={logo} className="nav-logo" alt="logo"/>
+
+                <ul className={'nav-header-items'}>
+                    <li onClick={() => {
+                        alert('Pricing Clicked')
+                    }}
+                    >Pricing
+                    </li>
+                    <li
+                        onClick={() => {
+                            alert('About Clicked')
+                        }}>About
+                    </li>
+                    <li onClick={() => {
+                        alert('Contact Clicked')
+                    }}>Contact
+                    </li>
+                </ul>
             </nav>
-        </header>
+        </header>)
+}
 
-        <h1>Fun fact about React</h1>
+function MainContent() {
+    return (<div className={'main-content'}>
+            <h1>Fun fact about React</h1>
 
-        <ul>
-            <li>Was released in 2013</li>
-            <li>Was originally created by JORDAN WALKE</li>
-            <li>Has well over 100k starts on GitHub</li>
-            <li>It is maintained by Facebook</li>
-            <li>Power thousands of enterprise apps, including mobile apps</li>
-        </ul>
+            <ol>
+                <li>Was released in 2013</li>
+                <li>Was originally created by JORDAN WALKE</li>
+                <li>Has well over 100k starts on GitHub</li>
+                <li>It is maintained by Facebook</li>
+                <li>Power thousands of enterprise apps, including mobile apps</li>
+            </ol>
+        </div>)
+}
 
-        <footer>
+function Footer() {
+    return (
+
+        <footer className={'footer'}>
             <small> Thanks for watching</small>
         </footer>
 
-    </div>);
+    )
 }
 
 export default App
