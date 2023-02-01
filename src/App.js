8c31b831993aa691f0da7fd1df027af15d4d611a
Pratik Fagadiya <pratikfagadiya@gmail.com>
2023-02-01 16:58:45 +0530

    Setup dark and light theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
 
 const App = () => {
-    return (<div>
+
+    const [darkTheme, setDarkTheme] = useState(true)
+
+    function changeTheme() {
+        setDarkTheme(prevState => !prevState)
+    }
+
+    return (<div className={darkTheme ? 'dark-theme' : "light-theme"}>
+        <button onClick={changeTheme}>Change Theme</button>
         <Header/>
         <MainContent/>
         <Footer/>
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import React from "react";
+import './App.css'
 
 export default function Header() {
     return (<header>
