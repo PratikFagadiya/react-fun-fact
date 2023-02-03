4ddf99d46ad389e84980399c4a40c6faf4f9ded9
Pratik Fagadiya <pratikfagadiya@gmail.com>
2023-02-03 19:40:13 +0530

    Init config and add variant in .env file

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const dotenv = require('dotenv');
 
-const PORT = 4000
+dotenv.config();
+
+const PORT = process.env.PORT || 4000
 
 app.use(cors())
 app.use(express.json())
